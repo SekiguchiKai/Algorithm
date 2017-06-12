e658056eadc65ea2c9b05a931918ec76bc23e009
sekky0905 <ks.sekiguchi.0905@gmail.com>
2017-06-12 22:37:15 +0900

    add InsertSort Logic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());
+        String A = in.readLine();
+
+        Sort.insertionSort(A, N);
+
     }
 }
